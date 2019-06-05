1a0ab526fbb75c01129d269036716c8102d5b811
Travis Exline <t.exline22@gmail.com>
2019-06-05 20:29:15 +0000

    scraper progress

--- lib/computers/scraper.rb
+++ lib/computers/scraper.rb
@@ -1,24 +1,14 @@
 require 'open-uri'
 
 class Computers::Scraper
-  attr_accessor :pc
-  attr_reader :doc
-
-  def initialize
-    @pc = Computers::PCs.new
-  end
-
   def get_page
-    @doc = Nokogiri::HTML(open("https://techradar.com/news/computing/pc/10-of-the-best-gaming-pcs-you-can-buy-in-2015-1304263"))
+    Nokogiri::HTML(open("https://techradar.com/news/computing/pc/10-of-the-best-gaming-pcs-you-can-buy-in-2015-1304263"))
   end
 
-  def scrape
-    scrape_details
-    @pc
-  end
-
-  def scrape_details
+  def specs
     pc_hash = {}
-    doc.css("h3").each {|pc| pc_hash[pc.to_sym] = {}}
+    specs = []
+    get_page.css("h3").each {|pc| pc_hash[pc.text.to_sym] = {}}
+    get_page.css("p.specs__container#text")
   end
 end
